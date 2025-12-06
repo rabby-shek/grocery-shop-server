dde880836d7dc873f4d741e65fc0938d51ac7d00
rabby-shek <rabbyshek1998@gmail.com>
2025-12-06 12:46:13 +0600

    Implement user registration with OTP verification and email notifications

---
+++ src/models/user.model.js
@@ -0,0 +1,50 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    otp: {
+      type: String,       // store OTP code
+      default: null,
+    },
+
+    otpExpiresAt: {
+      type: Date,         // expiry time
+      default: null,
+    },
+
+    isVerified: {
+      type: Boolean,
+      default: false,     // user must verify OTP first
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
