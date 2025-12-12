aea77d90affb3a55cea3045963c7f46743c15ca4
rabby-shek <rabbyshek1998@gmail.com>
2025-12-12 21:00:39 +0600

    update category route api added

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,7 +1,10 @@
 import Categories from "../models/category.model.js";
 import Product from "../models/product.model.js";
 
+// ================
 // Create category
+// ================
+
 export const createCategory = async (req, res) => {
     try {
         const { categoryName } = req.body;
@@ -29,15 +32,115 @@ export const createCategory = async (req, res) => {
     }
 };
 
+
+// ===================
+// Get All Categories
+// ===================
+
+export const getAllCategories = async (req, res) => {
+    try {
+        const categories = await Categories.find();
+
+        res.status(200).json({
+            success: true,
+            data: categories,
+            message: "Successfully fetched all categories data"
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Error fetching categories data"
+        })
+    }
+}
+
+
+// ====================
+// Get Single category
+// ====================
+
+export const getSingleCategory = async (req, res) => {
+    try {
+        const category = await Categories.findById(req.params.id);
+
+        if (!category) {
+            return res.status(404).json({
+                success: false,
+                message: "Category not found",
+            });
+        }
+        res.status(200).json({
+            success: true,
+            data: category,
+            message: "Category found successfully."
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Error fetching category data"
+        })
+    }
+}
+
+// =================
+// Update Category
+// =================
+
+export const updateCategory = async (req, res) => {
+    try {
+        const { categoryName } = req.body;
+
+        const imagePath = req.file
+            ? `/uploads/categories/${req.file.filename}`
+            : undefined;
+
+        const updateData = { categoryName };
+
+        if (imagePath) {
+            updateData.image = imagePath;
+        }
+
+        const updatedCategory = await Categories.findByIdAndUpdate(
+            req.params.id,
+            updateData,
+            { new: true }
+        );
+
+        if (!updatedCategory) {
+            return res.status(404).json({
+                success: false,
+                message: "Category not found"
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Category updated successfully",
+            data: updatedCategory
+        });
+
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Failed to update category",
+            error: error.message
+        });
+    }
+}
+
+// ==============
 // Create product
+// ==============
 
 export const createProduct = async (req, res) => {
     try {
         const { name, price, description, category } = req.body;
-        console.log(req.body);
+
         const imagePath = req.file
             ? `/uploads/products/${req.file.filename}`
             : undefined;
+
         const product = await Product.create({
             name,
             price,
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createCategory, createProduct } from '../controllers/product.controller.js';
+import { createCategory, createProduct, getAllCategories, getSingleCategory, updateCategory } from '../controllers/product.controller.js';
 import dynamicUploader from '../middlewares/uploadimage.js';
 
 const router = express.Router();
@@ -7,6 +7,16 @@ const router = express.Router();
 const uploadProductImage = dynamicUploader('products');
 const uploadCategoryImage = dynamicUploader('category');
 
+// ==========================
+// All categories routes
+// ==========================
 router.post('/create-category', uploadCategoryImage.single('image'), createCategory);
+router.get('/get-all-categories', getAllCategories);
+router.get('/get-single-category/:id', getSingleCategory);
+router.put('/update-category/:id', uploadCategoryImage.single('image'), updateCategory);
+
+// ====================
+// All products route
+// ====================
 router.post('/create-product', uploadProductImage.single('image'), createProduct);
 export default router;
